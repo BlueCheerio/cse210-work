152008b6bc23dccfff1ccc0bc36c38da359e0f56
Austin Cope <atcope03@gmail.com>
2026-01-15 14:47:08 -0700

    Prep 3 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        bool playing = true;
+        while (playing)
+        {
+            playing = false;
+            bool guessing = true;
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 101);
+            Console.WriteLine("Welcome to the number guessing game! You get to guess a number between 1-100");
+            while (guessing)
+            {
+                Console.WriteLine("What is your guess? ");
+                string answer = Console.ReadLine();
+                int guess = int.Parse(answer);
+                if (guess > number)
+                {
+                    Console.WriteLine("lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("higher");
+                }
+                else if (guess == number)
+                {
+                    Console.WriteLine("Congratulations you guessed it!");
+                    guessing = false;
+                }
+                else
+                {
+                    Console.WriteLine("We're not sure what number that is");
+                }
+            }
+            Console.WriteLine("\nWould you like to play again? (Y/N)");
+            string again = Console.ReadLine();
+            if (again == "Y" || again == "y")
+            {
+                playing = true;
+            }
+            else
+            {
+                Console.WriteLine("\nThanks for playing!");
+            }
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Diagnostics.Contracts;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        string weight = "kg/lb";
+        string temp = "farenheit/celsius";
+        string distance = "meter/foot";
+        bool dcont = true;
+        while (dcont)
+        {
+            Console.WriteLine("Welcome to the unit converter, please select a conversion (write the exact answer):");
+            Console.WriteLine(weight);
+            Console.WriteLine(temp);
+            Console.WriteLine(distance);
+            string answer = Console.ReadLine();
+            if (answer != weight || answer != temp || answer != distance)
+            {
+                Console.WriteLine("Please input the exact answer!");
+            }
+            else
+            {
+                dcont = false;
+            }
+        }
+        
+
     }
 }
