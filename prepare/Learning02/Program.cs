43d9b3d05076761a51ca15fa2c23f78a1b53ec6c
Austin Cope <atcope03@gmail.com>
2026-01-26 18:46:22 -0700

    Learning02 complete

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job = new Job();
+        job._jobTitle = "Garbage Man";
+        job._company = "Garbage Inc.";
+        job._startYear = 1492;
+        job._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Super Smart Guy";
+        job2._company = "Smarty Pants Co.";
+        job2._startYear = 2026;
+        job2._endYear = 2027;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Austin Cope";
+        resume1._job.Add(job);
+        resume1._job.Add(job2);
+        resume1.Display();
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,11 +6,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        for (int i = 0; i < 10; i++)
-        {
-            Console.WriteLine(10-i);
-        }
-        
-
+        int largest = FindLargeNumber();
+        Console.WriteLine($"The largest number is: {largest}");
     }
+    static int FindLargeNumber()
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished: ");
+            bool enter = true;
+            List<int> numbers = new List<int>();
+            while (enter)
+            {
+                Console.WriteLine("Enter Number: ");
+                string read = Console.ReadLine();
+                if (read != "0")
+                {
+                    int numberinput = int.Parse(read);
+                    numbers.Add(numberinput);
+                }
+                else
+                {
+                    enter = false;
+                }
+            }
+            int largest = 0;
+            foreach (int number in numbers)
+            {
+                if (number > largest)
+                {
+                    largest = number;
+                }
+            }
+            return largest;
+        }
 }
