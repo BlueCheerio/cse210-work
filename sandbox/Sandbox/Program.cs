e2bc8ee538719af0452124bf1c8888a4c3a0ff9a
Austin Cope <atcope03@gmail.com>
2026-02-05 14:51:19 -0700

    Let's pray that Develop 2 works

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,36 +6,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        int largest = FindLargeNumber();
-        Console.WriteLine($"The largest number is: {largest}");
+        Car myCar = new Car();
+        myCar.Run();
     }
-    static int FindLargeNumber()
-        {
-            Console.WriteLine("Enter a list of numbers, type 0 when finished: ");
-            bool enter = true;
-            List<int> numbers = new List<int>();
-            while (enter)
-            {
-                Console.WriteLine("Enter Number: ");
-                string read = Console.ReadLine();
-                if (read != "0")
-                {
-                    int numberinput = int.Parse(read);
-                    numbers.Add(numberinput);
-                }
-                else
-                {
-                    enter = false;
-                }
-            }
-            int largest = 0;
-            foreach (int number in numbers)
-            {
-                if (number > largest)
-                {
-                    largest = number;
-                }
-            }
-            return largest;
-        }
 }
