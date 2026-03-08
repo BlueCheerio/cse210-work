2a7d1dd702ac9fa33f13387840a7b8ff8a0625bb
Austin Cope <atcope03@gmail.com>
2026-03-07 20:18:46 -0700

    deleted a comment I no longer need

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,7 +1,4 @@
 using System.Runtime.CompilerServices;
-
-//I know I could have made this class more useful. I decided to only use it for getters and setters though, please don't dock me for it
-//I am aware that I could have created a new instance of the class for each reference in the AllScriptures dictionary or just one instance when we created a new scripture
 public class Reference
 {
     private string _reference;
