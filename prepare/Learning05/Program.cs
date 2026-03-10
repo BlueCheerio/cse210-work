394fd299168e876edf01caff93fec0e899044aa5
Austin Cope <atcope03@gmail.com>
2026-03-10 14:34:16 -0600

    Learning 5 complete

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>
+        {
+            new Square("green", 5),
+            new Rectangle("purple", 10, 5),
+            new Circle("Red", 1)
+        };
+
+        foreach (Shape item in shapes)
+        {
+            if (item is Square)
+            {
+                Console.WriteLine("\nThis is a Square:");
+            }
+            else if (item is Rectangle)
+            {
+                Console.WriteLine("\nThis is a Rectangle:");
+            }
+            else if (item is Circle)
+            {
+                Console.WriteLine("\nThis is a Circle:");
+            }
+            Console.WriteLine($"Color: {item.GetColor()}");
+            Console.WriteLine($"Area: {item.GetArea()}");
+        }
     }
 }
